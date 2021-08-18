d260c37a17ed1054be00d28a6b9528dc2f8c2351
name <varunbrahmane30@gmail.com>
2021-08-18 19:41:02 +0530

    [varun] added: main program for ado.net

---
+++ AddressBookUsingADO.NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookUsingADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to address book using ADO.NET");
+        }
+    }
+}
